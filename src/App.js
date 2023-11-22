b315d9fc914fc66c43ccadcf755c63fae39694b9
Jolie92 <lauraloriga92@gmail.com>
2023-11-22 15:03:59 +0000

    added form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>      
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+        This project was coded by <a href="https://myportfoliolauraloriga.netlify.app/"
+        target="_blank" rel="noreferrer">Laura Loriga </a>  and is {""}
+        <a href="https://github.com/Jolie92/dictionary-app"
+        target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
+      </footer>
+    </div>
     </div>
   );
 }
