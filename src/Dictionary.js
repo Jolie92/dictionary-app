184d48a6aa80ddfa35ae0caf23f464e3aeb9c8fa
Jolie92 <lauraloriga92@gmail.com>
2023-11-23 10:10:04 +0000

    added meaning,synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,23 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Result from "./Result";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [result, setResult] = useState({});
+   
 
 function handleResponse(response){
      console.log(response.data[0]);
+     setResult(response.data[0]);
 }
 
 function search(event) {
         event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello${keyword}`;
-        console.log(apiUrl);
+        let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -26,6 +30,7 @@ return(
 <form onSubmit={search}>
     <input type="search" onChange={handleKeywordChange}/>
 </form>
+<Result result={result}/>
 </div>
 );
 }
