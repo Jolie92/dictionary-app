cebbaffbee294e445ca08f342da20c6940a32f16
Jolie92 <lauraloriga92@gmail.com>
2023-11-23 08:19:26 +0000

    added axios, Api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,24 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
 
-    function search(event) {
+function handleResponse(response){
+     console.log(response.data[0]);
+}
+
+function search(event) {
         event.preventDefault();
-        alert(`searching for ${keyword} definition`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 
-    function handleKeywordChange(event){
-setKeyword(event.target.value);
+function handleKeywordChange(event){
+     setKeyword(event.target.value);
 
     }
 return(
